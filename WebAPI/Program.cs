00cf2fc76dd706ddb49c7dd5a7fe3f9fc414f70a
DavidEggenberger <david99.eggenberger@gmail.com>
2021-11-20 09:30:16 +0100

    Azure Key Vault

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +21,11 @@ namespace WebAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        config.AddAzureKeyVault(new Uri("https://teamturtlekeyvault.vault.azure.net/"),
+                            new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = "439c3c1e-b179-447d-88c6-d7b6e8a08232" }));
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }

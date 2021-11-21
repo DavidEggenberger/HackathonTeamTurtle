7c263d6e16e86c3366209ba6218b08664bb5eaee
DavidEggenberger <david99.eggenberger@gmail.com>
2021-11-21 13:19:35 +0100

    Editform

--- DTOs/LearningPathDTO.cs
+++ DTOs/LearningPathDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DTOs
@@ -8,7 +9,9 @@ namespace DTOs
     {
         public string UserId { get; set; }
         public Guid Id { get; set; }
+        [MinLength(3, ErrorMessage = "Minimum Length 3 characters")]
         public string Name { get; set; }
+        [Range(1, 99, ErrorMessage = "Must be between 1-99hrs")]
         public int EstimatedCompletionTimeInHrs { get; set; }
         public int EnrolledUsersCount { get; set; }
         public string Genre { get; set; }
--- DTOs/LearningRessourceDTO.cs
+++ DTOs/LearningRessourceDTO.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DTOs
 {
     public class LearningRessourceDTO
     {
         public string Name { get; set; }
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Link is required")]
         public string LinkURI { get; set; }
         public bool IsVideo { get; set; }
         public List<QuizQuestionDTO> QuizQuestions { get; set; }
